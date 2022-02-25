fb037017bcdb3a7591d4ece080d783cc30a0e879
seungwan-jung <saddler-tenure0i@icloud.com>
2022-02-26 01:22:56 +0900

    HttpServletResponse

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,35 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet",urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[response-headers]
+        response.setHeader("Content-Type","text/plain;charset=utf-8");
+        response.setHeader("Cashe-Control","no-cache,no-store,must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setHeader("my-header","hello");
+
+        //[Header 편의 메서드
+        content(response);
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+}
